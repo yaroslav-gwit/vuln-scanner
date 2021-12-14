060ed388cd1027e1b9b4afc6486c18c4e9d6bfe6
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-14 13:57:59 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ $UID != 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+if [[ $(which curl 2>/dev/null | wc -l) == 0 ]]; then echo "Please install curl" && exit 1; fi
+if [[ $(which tar 2>/dev/null | wc -l) == 0 ]]; then echo "Please install tar" && exit 1; fi
+
+if [[ $(which grype 2>/dev/null | wc -l) == 0 ]]
+then
+    curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /bin
+else
+    echo "grype is already installed, skiping"
+    echo ""
+fi
+
+if [[ $(which syft 2>/dev/null | wc -l) == 0 ]]
+then
+    curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /bin
+else
+    echo "syft is already installed, skiping"
+    echo ""
+fi
+
+syft dir:/ -o json > ./wholesystem-$(date +%Y-%m-%d).json
+grype sbom:./wholesystem-$(date +%Y-%m-%d).json | grep -i log4j | tee vuln_output-$(date +%Y-%m-%d).txt

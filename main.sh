4b8cc447f3b3677618cb3932001eb59538dbcbb0
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-14 14:57:46 +0000

    Replaced $HOSTNAME with $(hostname)

--- main.sh
+++ main.sh
@@ -24,8 +24,8 @@ else
 fi
 
 syft -q dir:/ -o json > ./wholesystem-package-scan-$(date +%Y-%m-%d).json
-grype -q sbom:./wholesystem-package-scan-$(date +%Y-%m-%d).json | grep -i log4j | tee $HOSTNAME-vuln_output-$(date +%Y-%m-%d).txt
+grype -q sbom:./wholesystem-package-scan-$(date +%Y-%m-%d).json | grep -i log4j | tee $(hostname)-vuln_output-$(date +%Y-%m-%d).txt
 echo ""
 
-echo "All done! Checkout the $HOSTNAME-vuln_output-$(date +%Y-%m-%d).txt file, and save it as a report"
+echo "All done! Checkout the $(hostname)-vuln_output-$(date +%Y-%m-%d).txt file, and save it as a report"
 echo ""

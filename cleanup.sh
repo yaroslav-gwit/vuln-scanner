f91e2cd3c6a631ed9095fedad5b03d890562cc8f
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-15 11:59:30 +0000

    Initial commit

---
+++ cleanup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+if [[ $UID != 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+
+if [[ $(which grype 2>/dev/null | wc -l) != 0 ]]
+then
+    echo "found grype, removing"
+    rm -f $(which grype 2>/dev/null) 2>/dev/null
+    rm -f /bin/grype 2>/dev/null
+    echo "found grype, removing: DONE"
+    echo ""
+else
+    echo "grype is not installed on this system"
+fi
+
+
+if [[ -d ~/.cache/grype/db/3/ ]]
+then
+    echo "found grype cached DB, removing"
+    rm -rf ~/.cache/grype/db/3/
+    echo "found grype cached DB, removing: DONE"
+fi
+
+
+if [[ $(which syft 2>/dev/null | wc -l) != 0 ]]
+then
+    echo "found syft, removing"
+    rm -f $(which syft 2>/dev/null) 2>/dev/null
+    rm -f /bin/syft 2>/dev/null
+    echo "found syft, removing: DONE"
+    echo ""
+else
+    echo "syft is not installed on this system"
+fi
+
+
+echo "We are done cleaning up."
+echo ""

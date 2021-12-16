aee5da89f826e0e8a82ddc3f40da8f27bd82a544
Yaroslav Koisa <yaroslav@gateway-it.com>
2021-12-16 11:19:07 +0000

    Updated the script to exclude some folders

--- main.sh
+++ main.sh
@@ -5,10 +5,12 @@ if [[ $UID != 0 ]]; then
 fi
 
 
+#_ CHECK DEPENDENCIES _#
 if [[ $(which curl 2>/dev/null | wc -l) == 0 ]]; then echo "Please install curl" && exit 1; fi
 if [[ $(which tar 2>/dev/null | wc -l) == 0 ]]; then echo "Please install tar" && exit 1; fi
 
 
+#_ INSTALL SYFT AND GRYPE _#
 if [[ $(which grype 2>/dev/null | wc -l) == 0 ]]
 then
     curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /bin
@@ -16,7 +18,6 @@ else
     echo "grype is already installed, skipping"
     echo ""
 fi
-
 
 if [[ $(which syft 2>/dev/null | wc -l) == 0 ]]
 then
@@ -27,15 +28,39 @@ else
 fi
 
 
-syft -q dir:/ -o json > ./wholesystem-package-scan-$(date +%Y-%m-%d).json
-echo "Host: $(hostname)" > $(hostname)_vuln_output-$(date +%Y-%m-%d).txt
-echo "" >> $(hostname)_vuln_output-$(date +%Y-%m-%d).txt
-echo "List of vulnerabilities found:" >> $(hostname)_vuln_output-$(date +%Y-%m-%d).txt
-grype -q sbom:./wholesystem-package-scan-$(date +%Y-%m-%d).json | grep -i log4j | tee -a $(hostname)_vuln_output-$(date +%Y-%m-%d).txt
-echo "" >> $(hostname)_vuln_output-$(date +%Y-%m-%d).txt
-echo "" >> $(hostname)_vuln_output-$(date +%Y-%m-%d).txt
+#_ RENAME SYFT INDEXING RESULT FILES TO OLD, SO THAT THE VULNERABILITY REPORT WILL BE UP TO DATE _#
+for JSON_RESULT in $(ls *_packagescan_*)
+do
+    mv ${JSON_RESULT} ${JSON_RESULT}.OLD
+done
+
+
+#_ START SYFT INDEXING EXCLUDING SOME FOLDERS _#
+DIRS_TO_SCAN=$(ls -l / | grep '^d' | grep -vi 'lost+found\|mnt\|boot\|dev\|media' | awk '{ print $NF }')
+for DIR in $DIRS_TO_SCAN
+do
+    SYFT_FILE_NAME=${DIR}_packagescan_$(date +%Y-%m-%d).json
+    syft -q dir:/${DIR} -o json > ./${SYFT_FILE_NAME}
+done
+
+
+#_ CHECK INDEXED LIBRARIES FOR VULNERABILITIES _#
+RESULTS_FILENAME=$(hostname)_vuln_output-$(date +%Y-%m-%d).txt
+echo "Host: $(hostname)" > ${RESULTS_FILENAME}
+echo "" >> ${RESULTS_FILENAME}
+echo "List of vulnerabilities found:" >> ${RESULTS_FILENAME}
+
+JSONS_TO_SCAN=$(ls *_packagescan_* | grep -v ".OLD")
+for JSON in JSONS_TO_SCAN
+do
+    echo ""
+    echo "File scanned: ${JSON}">> ${RESULTS_FILENAME}
+    grype -q sbom:./${JSON} | grep -i log4j | tee -a ${RESULTS_FILENAME}
+
+echo "" >> ${RESULTS_FILENAME}
 echo ""
 
 
-echo "All done! Checkout the $(hostname)_vuln_output-$(date +%Y-%m-%d).txt file, and save it as a report"
+#_ FINAL MESSAGE BEFORE EXIT _#
+echo "All done! Checkout the ${RESULTS_FILENAME} file, and save it as a report"
 echo ""
